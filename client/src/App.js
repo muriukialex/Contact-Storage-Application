ccb95b4e783c78ea2bfeb0bafb48d0c90d490cd1
alex <alexmuriukimaina254@gmail.com>
2021-10-26 01:20:39 +0300

    added a react client folder

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      Here we are!
+      <p>Is not this amazing! no it's not</p>
+    </div>
+  );
+};
+
+export default App;
